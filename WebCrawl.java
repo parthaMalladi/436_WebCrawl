588aec1b740f518c6c70a374c6fb6abd17a83eec
parthaMalladi <partha.malladi@gmail.com>
2025-01-26 02:24:47 +0000

    saving work

--- WebCrawl.java
+++ WebCrawl.java
@@ -1,5 +1,26 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebCrawl {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        URL url = new URL("https://en.wikipedia.org/wiki/Northrop_B-2_Spirit");
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        if (responseCode == HttpsURLConnection.HTTP_OK) {
+            StringBuilder sb = new StringBuilder();
+            Scanner scanner = new Scanner(connection.getInputStream());
+
+            while (scanner.hasNext()) {
+                sb.append(scanner.nextLine()).append("\n");
+            }
+
+            System.out.println(sb);
+        }
     }
 }
