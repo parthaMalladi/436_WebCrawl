7e3a3d8fe08bc3c615b002f70125c067f1109054
parthaMalladi <partha.malladi@gmail.com>
2025-01-27 05:12:30 +0000

    error handling

--- WebCrawl.java
+++ WebCrawl.java
@@ -1,8 +1,10 @@
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,23 +19,38 @@ public class WebCrawl {
 
         String url = args[0];
         int hops = Integer.parseInt(args[1]);
+        if (hops < 0) {
+            hops = 0;
+        }
         Set<String> visited = new HashSet<>();
 
         String s = getHTML(url);
 
-        for (int i = 0; i < hops; i++) {
-            if (s.equals("invalid")) {
-                System.out.println("Invalid Request or Malformed URL");
+        for (int i = 0; i <= hops; i++) {
+            if (s.equals("") || s.substring(0,7).equals("invalid")) {
+                System.out.println("Invalid Request or Malformed URL: " + url);
+
+                if (!s.equals("")) {
+                    System.out.println("Error Code: " + s.substring(7));
+                }
+                break;
+            }
+
+            if (visited.contains(url)) {
+                System.out.print("Already Visited: " + url);
                 break;
             }
 
             visited.add(url);
+            System.out.println("Hop #" + i + " - Visiting: " + url);
             String newURL = extractURL(s, visited);
             
             if (!newURL.equals("")) {
                 url = newURL;
-                System.out.println(url);
                 s = getHTML(url);
+            } else {
+                System.out.println("No URLs on the webpage");
+                break;
             }
         }
     }
@@ -41,13 +58,19 @@ public class WebCrawl {
     private static String getHTML(String link) {
         try {
             URL url = new URL(link);
-            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            
+            URLConnection connection = url.openConnection();
 
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
+            if (link.charAt(4) == 's') {
+                connection = (HttpsURLConnection) connection;
+            } else {
+                connection = (HttpURLConnection) connection;
+            }
 
-            if (responseCode == HttpsURLConnection.HTTP_OK) {
+            ((HttpURLConnection) connection).setRequestMethod("GET");
+            int responseCode = ((HttpURLConnection) connection).getResponseCode();
+
+            if (responseCode == 200) {
                 StringBuilder sb = new StringBuilder();
                 Scanner scanner = new Scanner(connection.getInputStream());
 
@@ -57,7 +80,7 @@ public class WebCrawl {
                 scanner.close();
                 return sb.toString();
             } else {
-                return "invalid";
+                return "invalid" + responseCode;
             }
         } catch (MalformedURLException e) {
             System.out.println("Malformed URL: " + e.getMessage());
@@ -80,6 +103,7 @@ public class WebCrawl {
             if (mem.contains(ans)) {
                 continue;
             } else {
+                System.out.println("Extracted URL is: " + ans);
                 break;
             }
         }
