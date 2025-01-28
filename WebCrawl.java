4c91f442e58c34271be91f50c731b77bbcedd806
parthaMalladi <partha.malladi@gmail.com>
2025-01-28 02:22:50 +0000

    complete

--- WebCrawl.java
+++ WebCrawl.java
@@ -1,3 +1,5 @@
+// Partha Malladi
+
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -12,6 +14,7 @@ import java.util.regex.*;
 
 public class WebCrawl {
     public static void main(String[] args) {
+        // gets the URL and the number of hops from the user and checks for errors
         if (args.length != 2) {
             System.out.println("Usage: java WebCrawl <starting URL> <num_hops>");
             return;
@@ -24,9 +27,12 @@ public class WebCrawl {
         }
         Set<String> visited = new HashSet<>();
 
+        // extracts the HTML using a GET request
         String s = getHTML(url);
 
+        // for loop for each hop
         for (int i = 0; i <= hops; i++) {
+            // if getHTML() returns an error, print the error and break
             if (s.equals("") || s.substring(0,7).equals("invalid")) {
                 System.out.println("Invalid Request or Malformed URL: " + url);
 
@@ -36,15 +42,18 @@ public class WebCrawl {
                 break;
             }
 
+            // if URL has already been visited, break
             if (visited.contains(url)) {
                 System.out.print("Already Visited: " + url);
                 break;
             }
 
+            // add current URL to set and extract the next URL to hop to
             visited.add(url);
             System.out.println("Hop #" + i + " - Visiting: " + url);
             String newURL = extractURL(s, visited);
             
+            // if webpage has no other URLs to hop to, break. Otherwise get the HTML content of the new webpage
             if (!newURL.equals("")) {
                 url = newURL;
                 s = getHTML(url);
@@ -55,12 +64,14 @@ public class WebCrawl {
         }
     }
 
+    // helper function to get the HTML body from the URL
     private static String getHTML(String link) {
         try {
             URL url = new URL(link);
             
             URLConnection connection = url.openConnection();
 
+            // adjust connection based on HTTP or HTTPS request
             if (link.charAt(4) == 's') {
                 connection = (HttpsURLConnection) connection;
             } else {
@@ -70,6 +81,7 @@ public class WebCrawl {
             ((HttpURLConnection) connection).setRequestMethod("GET");
             int responseCode = ((HttpURLConnection) connection).getResponseCode();
 
+            // if a valid request, return the body. Otherwise return 'invalid' and the error code
             if (responseCode == 200) {
                 StringBuilder sb = new StringBuilder();
                 Scanner scanner = new Scanner(connection.getInputStream());
@@ -93,11 +105,13 @@ public class WebCrawl {
         return "";
     }
 
+    // helper function that uses regex to find http and https links
     private static String extractURL(String htmlContent, Set<String> mem) {
         Pattern pattern = Pattern.compile("<a\\s+href\\s*=\\s*\"(http[^\"]+)\"");
         Matcher matcher = pattern.matcher(htmlContent);
         String ans = "";
 
+        // keep looking for a URL that hasn't already been visited
         while (matcher.find()) {
             ans = matcher.group(1);
             if (mem.contains(ans)) {
